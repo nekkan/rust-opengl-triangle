85f87e856dcb16b26ddeac63d2d22769645a5a04
nekkan <nxkkdev@gmail.com>
2020-11-30 18:26:42 -0300

    Change binding generator to ProfilingStructGenerator at build.rs.

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,6 +1,7 @@
 extern crate gl_generator;
 
-use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
+use gl_generator::{Api, Fallbacks, Profile, Registry};
+use gl_generator_profiling_struct::ProfilingStructGenerator;
 use std::env;
 use std::fs::File;
 use std::path::Path;
@@ -11,7 +12,15 @@ fn main() {
     let dest = env::var("OUT_DIR").unwrap();
     let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
 
-    Registry::new(Api::Gl, (4, 6), Profile::Core, Fallbacks::All, [])
-        .write_bindings(GlobalGenerator, &mut file)
+    let registry = Registry::new(
+        Api::Gl,
+        (4, 6),
+        Profile::Core,
+        Fallbacks::All,
+        ["GL_NV_command_list"]
+    );
+
+    registry
+        .write_bindings(ProfilingStructGenerator, &mut file)
         .unwrap();
 }
