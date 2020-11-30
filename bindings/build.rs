558325528a8e24845befece9ad531c92e58058ad
nekkan <nxkkdev@gmail.com>
2020-11-30 18:52:54 -0300

    Remove gl_generator_profiling_struct dependency.

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,26 +1,26 @@
 extern crate gl_generator;
 
-use gl_generator::{Api, Fallbacks, Profile, Registry};
-use gl_generator_profiling_struct::ProfilingStructGenerator;
 use std::env;
 use std::fs::File;
 use std::path::Path;
 
-fn main() {
-    println!("Generating gl_bindings.rs file...");
+use gl_generator::{Api, Fallbacks, Profile, Registry, StructGenerator};
 
-    let dest = env::var("OUT_DIR").unwrap();
-    let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut file_gl = File::create(&Path::new(&out_dir).join("bindings.rs")).unwrap();
 
     let registry = Registry::new(
         Api::Gl,
         (4, 6),
         Profile::Core,
-        Fallbacks::All,
-        ["GL_NV_command_list"]
+        Fallbacks::All, [
+            "GL_NV_command_list",
+        ],
     );
 
-    registry
-        .write_bindings(ProfilingStructGenerator, &mut file)
-        .unwrap();
+    registry.write_bindings(
+        StructGenerator,
+        &mut file_gl,
+    ).unwrap();
 }
