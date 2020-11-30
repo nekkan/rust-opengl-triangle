23345e04ad1d331adff0d6e0c6e1707d075ad4b3
nekkan <nxkkdev@gmail.com>
2020-11-30 18:36:38 -0300

    Create lib.rs.

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::ops::Deref;
+use std::rc::Rc;
+
+pub use crate::bindings::*;
+
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+}
+
+#[derive(Clone)]
+pub struct Gl {
+    inner: Rc<bindings::Gl>,
+}
+
+impl Gl {
+    pub fn load_with<F: FnMut(&'static str) -> *const types::GLvoid>(loadfn: F) -> Gl {
+        Gl {
+            inner: Rc::new(bindings::Gl::load_with(loadfn)),
+        }
     }
 }
+
+impl Deref for Gl {
+    type Target = bindings::Gl;
+
+    fn deref(&self) -> &bindings::Gl {
+        &self.inner
+    }
+}
+
