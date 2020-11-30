49a6d638d3716e48481ea9834e1b58e21612870e
nekkan <nxkkdev@gmail.com>
2020-11-30 17:49:05 -0300

    Make build.rs warn when the file is going to be generated.

--- src/build.rs
+++ src/build.rs
@@ -6,6 +6,8 @@ use std::fs::File;
 use std::path::Path;
 
 fn main() {
+    println!("Generating gl_bindings.rs file...");
+
     let dest = env::var("OUT_DIR").unwrap();
     let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
 
