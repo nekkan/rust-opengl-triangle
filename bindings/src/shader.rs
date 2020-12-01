4225eb9a79f02250c363fb99f61f8e221384d9a5
nekkan <nxkkdev@gmail.com>
2020-12-01 14:17:42 -0300

    Fix parameterizing for Gl::ShaderSource at shader.rs.

--- bindings/src/shader.rs
+++ bindings/src/shader.rs
@@ -1,5 +1,6 @@
 use {
     crate::gl_bindings,
+    crate::gl_bindings::Gl,
     crate::gl_bindings::types::*,
     std::{
         ffi::{CStr, CString},
@@ -12,27 +13,27 @@ pub struct Shader {
 }
 
 impl Shader {
-    fn new(vertex_shader: &str, fragment_shader: &str) -> Shader {
+    fn new(gl: &Gl, vertex_shader: &str, fragment_shader: &str) -> Shader {
         let vertex_shader = CString::new(vertex_shader.as_bytes()).unwrap();
         let fragment_shader = CString::new(fragment_shader.as_bytes()).unwrap();
         let mut shader = Shader { id: 0 };
 
         unsafe {
-            let vertex = compile_shader(&vertex_shader, gl_bindings::VERTEX_SHADER);
-            check_for_compilation_errors(vertex, "VERTEX");
+            let vertex = compile_shader(gl, &vertex_shader, gl_bindings::VERTEX_SHADER);
+            check_for_compilation_errors(gl, vertex, "VERTEX");
 
-            let fragment = compile_shader(&fragment_shader, gl_bindings::FRAGMENT_SHADER);
-            check_for_compilation_errors(fragment, "FRAGMENT");
+            let fragment = compile_shader(gl, &fragment_shader, gl_bindings::FRAGMENT_SHADER);
+            check_for_compilation_errors(gl, fragment, "FRAGMENT");
 
-            let id = gl_bindings::CreateProgram();
-            gl_bindings::AttachShader(id, vertex);
-            gl_bindings::AttachShader(id, fragment);
-            gl_bindings::LinkProgram(id);
-            check_for_compilation_errors(id, "PROGRAM");
+            let id = gl.CreateProgram();
+            gl.AttachShader(id, vertex);
+            gl.AttachShader(id, fragment);
+            gl.LinkProgram(id);
+            check_for_compilation_errors(gl, id, "PROGRAM");
 
             // delete the shaders as they're linked into our program and no longer necessary
-            gl_bindings::DeleteShader(vertex);
-            gl_bindings::DeleteShader(fragment);
+            gl.DeleteShader(vertex);
+            gl.DeleteShader(fragment);
             shader.id = id;
         };
 
@@ -40,22 +41,22 @@ impl Shader {
     }
 }
 
-unsafe fn compile_shader(shader_code: &CString, shader_type: GLuint) -> GLuint {
-    let shader = gl_bindings::CreateShader(shader_type);
-    gl_bindings::ShaderSource(shader, shader_code.as_ptr(), ptr::null());
-    gl_bindings::CompileShader(shader);
+unsafe fn compile_shader(gl: &Gl, shader_code: &CString, shader_type: GLuint) -> GLuint {
+    let shader = gl.CreateShader(shader_type);
+    gl.ShaderSource(shader, 1, &shader_code.as_ptr(), ptr::null());
+    gl.CompileShader(shader);
     return shader;
 }
 
-unsafe fn check_for_compilation_errors(shader: u32, shader_type: &str) {
+unsafe fn check_for_compilation_errors(gl: &Gl, shader: u32, shader_type: &str) {
     let mut success = gl_bindings::FALSE as GLint;
     let mut information = Vec::with_capacity(1024);
     information.set_len(1024 - 1); // subtract 1 to skip the trailing null character
 
     let status = if shader_type != "PROGRAM" { gl_bindings::COMPILE_STATUS } else { gl_bindings::LINK_STATUS };
-    gl_bindings::GetShaderiv(shader, status, &mut success);
+    gl.GetShaderiv(shader, status, &mut success);
 
-    gl_bindings::GetShaderInfoLog(shader, 1024, ptr::null_mut(), information.as_mut_ptr() as *mut GLchar);
+    gl.GetShaderInfoLog(shader, 1024, ptr::null_mut(), information.as_mut_ptr() as *mut GLchar);
     println!("Error of type {}:\n{}", shader_type, str::from_utf8(&information).unwrap());
 }
 
