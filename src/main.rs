4fefc32db548618170fedbc3affb84db31e12a3d
nekkan <nxkkdev@gmail.com>
2020-12-01 15:29:23 -0300

    Change window resolution to 900x600 at main.rs.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod triangle;
 fn main() {
     let window_builder = WindowBuilder::new()
         .with_title("Rust OpenGL Triangle")
-        .with_inner_size(LogicalSize::new(900.0, 400.0));
+        .with_inner_size(LogicalSize::new(900.0, 600.0));
 
     let event_loop = EventLoop::new();
 
